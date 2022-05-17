91fa985543c715ccc4be79876823cf8191ef922c
Chaitan Aswar <aswar.chaitan@gmail.com>
2022-05-17 12:41:57 +0530

    first commit

---
+++ src/app/Services/api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  requestHeaders = new HttpHeaders().set('Content-Type', 'text')
+    .append('basic', '123456789');
+
+  getstoreApiurl = "https://growiseit.com/movemykart/API/index.php/storeTypes";
+
+  public getstore(): Observable<any> {
+
+
+    return this.http.get(this.getstoreApiurl, {
+      headers: this.requestHeaders
+    })
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from "./Services/api.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  Data: any;
+
+  constructor(
+    private api:ApiService
+  ){}
+
+  ngOnInit(): void {
+    this.getAllstore();
+  }
+
+getAllstore()
+
+{
+  this.api.getstore().subscribe({next:(res)=>
+  {
+    this.Data=res.data;
+    console.log(this.Data);
+    
+  }})
+}
+
+
   title = 'MoveMyCart';
 }
